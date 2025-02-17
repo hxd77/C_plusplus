8db3db3af2bba34ce42159767f104695a7c2674b
dong77 <2059496322@qq.com>
2025-02-17 17:59:56 +0800

    delete

--- Chapter2_9.cpp
+++
@@ -1,7 +0,0 @@
-//exp2_9：输出图案
-#include<iostream>
-using namespace std;
-int main()
-{
-    
-}
---
+++ test.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    typedef int hxd;
+    hxd a,b;
+    decltype(a) c=2;
+    cout<<c<<endl;
+    return 0;
+}
